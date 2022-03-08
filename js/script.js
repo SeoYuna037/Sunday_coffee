35862bee16ee6e0a56e171b0f06efa80e2c80cfb
SeoYuna <tjdbsk037@gmail.com>
2022-03-08 12:13:31 +0900

    Feat : 모달창 추가
    -닫기 버튼 적용 문제 해결해야함

--- js/script.js
+++ js/script.js
@@ -54,11 +54,61 @@ window.addEventListener("scroll",()=>{
         toTop.classList.remove("active");
     }
 })
+//.frame animation
 const comSection=document.querySelector(".frame");
 window.addEventListener("scroll",()=>{
     if(window.pageYOffset>1080){
         comSection.classList.add("active");
     }else{
         comSection.classList.remove("active");
+    }
+})
+
+// Modal
+// const modal=document.querySelector(".modal");
+// const closeBtn=modal.querySelector("button");
+// const closeChecked=modal.querySelector("#todaycheck");
+
+// const closeModal=()=>{
+//     modal.classList.add("hidden");
+// }
+// closeBtn.addEventListener("click",closeModal);
+const modal=document.querySelector(".modal");
+const closeModal=document.querySelector(".modal-btn");
+const getCookie=function(cname){
+    const name=cname+"=";
+    const ca=document.cookie.split(';');
+    for(let i=0; i<ca.length;i++){
+        const c=ca[i];
+        while(c.charAt(0)===' ') c=c.substring(1);
+        if(c.indexOf(name)!==-1) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+const setCookie=function(cname,cvalue,exdays){
+    const todayDate=new Date();
+    todayDate.setTime(todayDate.getTime()+(exdays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + todayDate.toUTCString();
+    document.cookie=cname+"="+cvalue+"; "+expires;
+};
+const couponClose=function(){
+    if(document.querySelector('input[name="todaycheck"]').checked===true){
+        setCookie("close","Y",1);
+    }
+    modal.style.display="none";
+}
+const cookiedata=document.cookie;
+if(cookiedata.indexOf("close=Y")<0){
+    modal.style.display="block";
+}else{
+    modal.style.display="none";
+}
+const modalClose= function(){
+    modal.style.display="none";
+}
+closeModal.addEventListener('click',function(){
+    couponClose();
+    if(document.querySelector('input[name="todaycheck"]').checked===false){
+        modalClose();
     }
 })
